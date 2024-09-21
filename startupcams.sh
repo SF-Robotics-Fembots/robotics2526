443747b80d7da087a36ff5788b694378df3d6c66
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-09-21 11:53:38 -0700

    save

--- startupcams.sh
+++ startupcams.sh
@@ -2,5 +2,5 @@ rmmod uvcvideo
 modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
 bash focusscript.sh
 bash camerarun.sh 0 & sleep 5 
-#bash camerarun.sh 2 & sleep 5 
+bash camerarun.sh 2 & sleep 5 
 #bash camerarun.sh 4 & sleep 5 
