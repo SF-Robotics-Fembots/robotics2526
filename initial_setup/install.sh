09e9c39a511154bf747bb379c30a99e507a3848a
kin tirumala <25tirumala@stfrancishs.org>
2024-11-23 14:52:33 -0800

    venv install.sh 3

--- initial_setup/install.sh
+++ initial_setup/install.sh
@@ -5,4 +5,4 @@ apt-get update && apt-get install pip
 
 
 #install the requirements.txt file
-/home/geneseas/robotics2425/venv/bin/python3 -m /home/geneseas/robotics2425/venv/bin/python3-pip install -r requirements.txt
+/home/geneseas/robotics2425/venv/bin/python3 -m /home/geneseas/robotics2425/venv/bin/pip3 install -r requirements.txt
