8853d3ae932ae04d89ff09d280a485b7b8028d33
kin tirumala <25tirumala@stfrancishs.org>
2024-11-25 17:38:27 -0800

    fixed the bottomside cameras thing

--- camera_bottomside/startupcams.sh
+++ camera_bottomside/startupcams.sh
@@ -3,9 +3,9 @@
 rmmod uvcvideo
 modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
 bash focusscript.sh
-bash /home/geneseas/robotics2425/camerarun.sh 0 & sleep 5 
-bash /home/geneseas/robotics2425/camerarun.sh 2 & sleep 5 
-bash /home/geneseas/robotics2425/camerarun.sh 4 & sleep 5 
-bash /home/geneseas/robotics2425/camerarun.sh 6 & sleep 5
-bash /home/geneseas/robotics2425/camerarun.sh 8 & sleep 5
-bash /home/geneseas/robotics2425/camerarun.sh 1 & sleep 5 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 0 & sleep 5 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 2 & sleep 5 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 4 & sleep 5 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 6 & sleep 5
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 8 & sleep 5
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 1 & sleep 5 
