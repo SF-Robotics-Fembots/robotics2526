86fffb19a84476bca96b8b2e89fbacf383674cb6
kin tirumala <25tirumala@stfrancishs.org>
2024-11-25 17:57:23 -0800

    adding focus stuff to cameras

--- camera_bottomside/startupcams.sh
+++ camera_bottomside/startupcams.sh
@@ -2,7 +2,7 @@
 
 rmmod uvcvideo
 modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
-bash focusscript.sh
+bash /home/geneseas/robotics2425/camera_bottomside/focusscript.sh
 bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 0 & sleep 3 
 bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 2 & sleep 3 
 bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 4 & sleep 3 
