6a9bbd7fd88bac7ae2f427ab6efea5c34b1602de
kin tirumala <25tirumala@stfrancishs.org>
2024-11-25 17:44:51 -0800

    left paige buecker's jersey number behind

--- camera_bottomside/startupcams.sh
+++ camera_bottomside/startupcams.sh
@@ -3,10 +3,9 @@
 rmmod uvcvideo
 modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
 bash focusscript.sh
-bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 0 & sleep 5 
-bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 2 & sleep 5 
-bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 4 & sleep 5 
-bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 6 & sleep 5
-bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 8 & sleep 5
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 0 & sleep 3 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 2 & sleep 3 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 4 & sleep 3 
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 6 & sleep 3
+bash /home/geneseas/robotics2425/camera_bottomside/camerarun.sh 8 & sleep 3
 bash /home/geneseas/ustreamer/ustreamer --device=/dev/video10 --host=192.168.1.99 --format=MJPEG --port=8090 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW
- & sleep 5 
