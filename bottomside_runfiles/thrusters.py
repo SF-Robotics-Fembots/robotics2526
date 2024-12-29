e6f07c2ec3628c2da7f66b0b4831525791ac5d14
kin tirumala <25tirumala@stfrancishs.org>
2024-12-28 17:57:33 -0800

    A HUNDRED

--- bottomside_runfiles/thrusters.py
+++ bottomside_runfiles/thrusters.py
@@ -25,7 +25,7 @@ def main(ip_server):
 	i2c = busio.I2C(board.SCL, board.SDA)
 	shield = adafruit_pca9685.PCA9685(i2c)
 	kit = ServoKit(channels=16)
-	shield.frequency = 96
+	shield.frequency = 100
 
 	thrusterChannel1 = shield.channels[15]
 	thrusterChannel2 = shield.channels[0]
