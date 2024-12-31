8634c4ad6603a6b466e34dca31be41d3e60826a5
kin tirumala <25tirumala@stfrancishs.org>
2024-12-31 12:22:33 -0800

    dasf

--- bottomside_runfiles/thrusters.py
+++ bottomside_runfiles/thrusters.py
@@ -246,7 +246,7 @@ def main(ip_server):
 				#goes through code two times
 				oldVertThrusterVals[vNum] = int((calcVertical(v_speed, vNum, vDirArray)))
 
-			clockArray = [-1, 1, 1, -1]
+			clockArray = [1, 1, 1, -1]
 			clockVertArray = [1, -1]
 
 			thrusterVals = [0, 0, 0, 0]
