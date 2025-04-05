15eec7c7508fbc99cbdd3a58e17146c5cccfd082
gabby rosario <25Rosario@stfrancishs.org>
2025-04-05 15:36:03 -0700

    roll 'skies

--- bottomside_runfiles/thrusters.py
+++ bottomside_runfiles/thrusters.py
@@ -21,7 +21,7 @@ def main(ip_server):
 
 	#rotation compensation
 	rot_comp = -0.08 #was -0.28
-	slide_comp = 0.2
+	slide_comp = 0.25
 
 	i2c = busio.I2C(board.SCL, board.SDA)
 	shield = adafruit_pca9685.PCA9685(i2c)
@@ -302,9 +302,9 @@ def main(ip_server):
 			finalVertDiff = abs(powerVertThrusterVals[1] - vert_off_value)
 			finalTotal = (finalHorDiff * 4) + (finalVertDiff * 2)
 			if (finalTotal != 0):
-				percent = (2100/finalTotal) #2400
+				percent = (2400/finalTotal) #2400
 				#finds percent to display how much we are exceeding power use (ex. exceeding power limit by 5%)
-				if (finalTotal > 2100): #was 1950, max 2934
+				if (finalTotal > 2400): #was 1950, max 2934
 					for thruster in range(0, 4):
 						Diff = powerThrusterVals[thruster] - horiz_off_value
 						newDiff = Diff * (percent)
