6ebd49912035ea758da82b8dff8cd0e426cde4c7
gabby rosario <25Rosario@stfrancishs.org>
2025-04-12 10:51:57 -0700

    new chnages

--- bottomside_runfiles/gripper.py
+++ bottomside_runfiles/gripper.py
@@ -4,7 +4,7 @@ import socket, json
 
 #variables
 #ip_address = "127.0.0.1" # 192.168.1.100
-ip_address = "192.168.1.100"
+#ip_address = "192.168.1.100"
 port = 40000
 #gripper gpos
 front_gripper = 14
@@ -54,4 +54,4 @@ def main(ip_address):
 
 #always remember to call the function
 if          __name__ == "__main__":
-    main(ip_address)
+    main()
