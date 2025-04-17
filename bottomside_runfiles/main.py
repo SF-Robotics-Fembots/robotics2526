dac24875958e6efcfffa8692f6e5359ceac0e52c
gabby rosario <25Rosario@stfrancishs.org>
2025-04-17 12:38:49 -0700

    update

--- bottomside_runfiles/main.py
+++ bottomside_runfiles/main.py
@@ -11,6 +11,7 @@ import socket
 #import threaded files
 import thrusters
 import gripper
+import pump
 
 # library setup
 pygame.init()
@@ -26,9 +27,13 @@ ip_server = "192.168.1.100"
 
 thrusterCode = threading.Thread(target=thrusters.main, args = (ip_server,))
 gripperCode = threading.Thread(target=gripper.main, args=(ip_server,))
+pumpCode = threading.Thread(target=pump.main, args=(ip_server,))
+
 
 thrusterCode.start()
 gripperCode.start()
+pumpCode.start()
 
 thrusterCode.join()
 gripperCode.join()
+pumpCode.join()
