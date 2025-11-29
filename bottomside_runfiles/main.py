5df5e748a38d7ad693a340f2c5a150b0f7a2f527
JoseLopezAM <angelamiles021@gmail.com>
2025-11-29 09:33:37 -0800

    change ip address

--- bottomside_runfiles/main.py
+++ bottomside_runfiles/main.py
@@ -14,6 +14,7 @@ import gripper
 import pump
 
 # library setup
+
 pygame.init()
 i2c = busio.I2C(board.SCL, board.SDA)
 shield = adafruit_pca9685.PCA9685(i2c)
@@ -22,7 +23,7 @@ shield.frequency = 100
 
 #global ip variable setup
 global ip_server
-ip_server = "192.168.1.100"
+ip_server = "192.168.1.68" #192.168.1.100
     
 
 thrusterCode = threading.Thread(target=thrusters.main, args = (ip_server,))
