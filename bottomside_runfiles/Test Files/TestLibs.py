0537f97e064d2f3c860c52a8be59aea7377836d6
gabby rosario <25Rosario@stfrancishs.org>
2025-11-29 16:15:53 -0800

    test lib

--- bottomside_runfiles/Test Files/TestLibs.py
+++ bottomside_runfiles/Test Files/TestLibs.py
@@ -1,3 +1,6 @@
+import gpiod
+print("VENV gpiod:",getattr(gpiod, "__version__", "NO VERSION"), gpiod.__file__)
+
 import lgpio
 print("lgio from:", lgpio.__file__)
 print("has gpiochip_open:", hasattr(lgpio,"gpiochip_open"))
