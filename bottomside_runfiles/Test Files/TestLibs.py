59a87e91e23939e3b10e61b66b867902b23d3277
gabby rosario <25Rosario@stfrancishs.org>
2025-11-29 16:26:31 -0800

    testlibs

--- bottomside_runfiles/Test Files/TestLibs.py
+++ bottomside_runfiles/Test Files/TestLibs.py
@@ -1,9 +1,2 @@
-import gpiod
-print("VENV gpiod:",getattr(gpiod, "__version__", "NO VERSION"), gpiod.__file__)
-
-import lgpio
-print("lgio from:", lgpio.__file__)
-print("has gpiochip_open:", hasattr(lgpio,"gpiochip_open"))
-
-import board, digitalio
-print("Board OK", board)
+import lgpio, gpiod
+from
