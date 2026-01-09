aa8799eb9c5f86ba7675f56b72464a579494e6ab
gabby rosario <25Rosario@stfrancishs.org>
2026-01-08 16:32:20 -0800

    comit thruster changes

--- bottomside_runfiles/TestEachThruster.py
+++ bottomside_runfiles/TestEachThruster.py
@@ -33,7 +33,6 @@ def main():
 	shield = adafruit_pca9685.PCA9685(i2c)
 	shield.external_clock = True #enable 25MHz external crystal
 	kit = ServoKit(channels=16)
-
 	shield.frequency = 96
 
 	thrusterChannel1 = shield.channels[8]
@@ -131,60 +130,61 @@ def main():
 		# main loop
 		#x_speed = x_speed[1:]
 		# loop to collect value for each thruster using horizontal calculation function
-		print("Testing Thruster 1")
-		input("Press Enter to continue...")
-		throttlePW = int(1800/10000*65536)
-		thrusterChannel1.duty_cycle = throttlePW
-		time.sleep(1)
-		throttlePW = int(1500/10000*65536)
-		thrusterChannel1.duty_cycle = throttlePW
-		time.sleep(1)
+		# print("Testing Thruster 1")
+		# input("Press Enter to continue...")
+		# throttlePW = int(1800/10000*65536)
+		# thrusterChannel1.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# throttlePW = int(1500/10000*65536)
+		# thrusterChannel1.duty_cycle = throttlePW
+		# time.sleep(.5)
 
-		print("Testing Thruster 2")
-		input("Press Enter to continue...")
-		throttlePW = int(1800/10000*65536)
-		thrusterChannel2.duty_cycle = throttlePW
-		time.sleep(1)
-		throttlePW = int(1500/10000*65536)
-		thrusterChannel2.duty_cycle = throttlePW
-		time.sleep(1)
+		# print("Testing Thruster 2")
+		# input("Press Enter to continue...")
+		# throttlePW = int(1800/10000*65536)
+		# thrusterChannel2.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# throttlePW = int(1500/10000*65536)
+		# thrusterChannel2.duty_cycle = throttlePW
+		# time.sleep(.5)
 
 		print("Testing Thruster 3")
 		input("Press Enter to continue...")
 		throttlePW = int(1800/10000*65536)
 		thrusterChannel3.duty_cycle = throttlePW
-		time.sleep(1)
+		time.sleep(.5)
 		throttlePW = int(1500/10000*65536)
 		thrusterChannel3.duty_cycle = throttlePW
-		time.sleep(1)
+		time.sleep(.5)
 
-		print("Testing Thruster 4")
-		input("Press Enter to continue...")
-		throttlePW = int(1800/10000*65536)
-		thrusterChannel4.duty_cycle = throttlePW
-		time.sleep(1)
-		throttlePW = int(1500/10000*65536)
-		thrusterChannel4.duty_cycle = throttlePW
-		time.sleep(1)
+		# print("Testing Thruster 4")
+		# input("Press Enter to continue...")
+		# throttlePW = int(1800/10000*65536)
+		# thrusterChannel4.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# throttlePW = int(1500/10000*65536)
+		# thrusterChannel4.duty_cycle = throttlePW
+		# time.sleep(.5)
 
-		print("Testing Thruster 5")
-		input("Press Enter to continue...")
-		throttlePW = int(1800/10000*65536)
-		thrusterChannel5.duty_cycle = throttlePW
-		time.sleep(1)
-		throttlePW = int(1500/10000*65536)
-		thrusterChannel5.duty_cycle = throttlePW
-		time.sleep(1)
+		# print("Testing Thruster 5")
+		# input("Press Enter to continue...")
+		# throttlePW = int(1800/10000*65536)
+		# thrusterChannel5.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# throttlePW = int(1500/10000*65536)
+		# thrusterChannel5.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# input("Press Enter to continue...")
 
-		print("Testing Thruster 6")
-		input("Press Enter to continue...")
-		throttlePW = int(1800/10000*65536)
-		thrusterChannel6.duty_cycle = throttlePW
-		time.sleep(1)
-		throttlePW = int(1500/10000*65536)
-		thrusterChannel6.duty_cycle = throttlePW
-		time.sleep(1)
-		input("Press Enter to continue...")
+		# print("Testing Thruster 6")
+		# input("Press Enter to continue...")
+		# throttlePW = int(1800/10000*65536)
+		# thrusterChannel6.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# throttlePW = int(1500/10000*65536)
+		# thrusterChannel6.duty_cycle = throttlePW
+		# time.sleep(.5)
+		# input("Press Enter to continue...")
 
 if __name__ == "__main__":
 	main()
