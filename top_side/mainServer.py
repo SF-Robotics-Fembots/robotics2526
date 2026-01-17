0abb599540dcb789ca77159a8245ed737e1156f8
Marcus Grindstaff <magrinds@gmail.com>
2026-01-17 15:23:37 -0800

    comment out gripper Server

--- top_side/mainServer.py
+++ top_side/mainServer.py
@@ -7,8 +7,8 @@ import threading
 #import the threaded files
 import joyServer
 import piServer
-import gripperServer
-import pumpServer  
+#import gripperServer
+#mport pumpServer  
 
 global ip_server
 ip_server = "192.168.1.67" #192.168.1.100
