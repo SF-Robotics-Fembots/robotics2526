04104155aa002923b97bf5ac341dba188c8ba3b1
27gonzales <star.anqel3@gmail.com>
2026-02-21 11:42:21 -0800

    WIP: local changes before pulling

--- bottomside_runfiles/camera_on.sh
+++ bottomside_runfiles/camera_on.sh
@@ -2,7 +2,8 @@
 
 #making a new bash script to run the camera GPIOs
 
-/home/geneseas/robotics2425/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/ ./gpio 0x0424 0x2734 0x01 2 1
-/home/geneseas/robotics2425/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/ ./gpio 0x0424 0x2734 0x01 3 1
-/home/geneseas/robotics2425/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/ ./gpio 0x0424 0x2734 0x01 7 1
-/home/geneseas/robotics2425/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/ ./gpio 0x0424 0x2734 0x01 9 1
+/home/geneseas/robotics2526/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/gpio 0x0424 0x2732 0x01 2 0
+/home/geneseas/robotics2526/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/gpio 0x0424 0x2732 0x01 3 1
+/home/geneseas/robotics2526/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/gpio 0x0424 0x2732 0x01 7 0
+/home/geneseas/robotics2526/Linux_USB57xx_58xx_59xx_ACE_V1.0/examples/gpio/out/gpio 0x0424 0x2732 0x01 9 1
+
