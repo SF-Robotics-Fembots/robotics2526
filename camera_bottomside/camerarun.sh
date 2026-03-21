0375f7a78de2d79b1beb63a250062afada335906
27gonzales <star.anqel3@gmail.com>
2026-03-21 14:43:35 -0700

    hi

--- camera_bottomside/camerarun.sh
+++ camera_bottomside/camerarun.sh
@@ -1,1 +1,20 @@
-ustreamer --device=/dev/video$1 --host=192.168.1.68 --format=MJPEG --port=808$1 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW -n
+#ustreamer --device=/dev/video$1 --host=192.168.1.68 --format=MJPEG --port=808$1 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW -n
+
+#!/bin/bash
+
+#!/bin/bash
+
+DEVICE=${1:-0}
+PORT=$((8080 + DEVICE))
+
+ustreamer \
+  --device=/dev/video$DEVICE \
+  --host=192.168.1.68 \
+  --format=MJPEG \
+  --port=$PORT \
+  --device-timeout 2 \
+  -r 800x600 \
+  -b 2 \
+  --workers 2 \
+  --encoder=HW \
+  -n
--- camera_bottomside/startupcams.sh
+++ camera_bottomside/startupcams.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-#bash cameraon.sh	
-#camera on by default 
-#rmmod uvcvideo
-#modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+bash cameraon.sh	
+
+#ok lets see if this fixes the error!
+# Kill any processes using cameras
+sudo fuser -k /dev/video0
+sudo fuser -k /dev/video2
+sudo fuser -k /dev/video4
+sudo fuser -k /dev/video6
+sudo fuser -k /dev/video8
+sudo fuser -k /dev/video10
+
+#camera on by default  2/28 i added sudo
+sudo rmmod uvcvideo
+sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+read -p "Press Enter"
 bash /home/geneseas/robotics2526/camera_bottomside/focusscript.sh
 bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 0 & sleep 3
 bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 2 & sleep 3
 bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 4 & sleep 3
-# bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 6 & sleep 3
-# bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 8 & sleep 3
-#/home/geneseas/ustreamer/ustreamer --device=/dev/video10 --host=192.168.1.99 --format=MJPEG --port=8090 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 6 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 8 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 10 & sleep 3
+#/home/geneseas/ustreamer/ustreamer --device=/dev/video10 --host=192.168.1.68 --format=MJPEG --port=8090 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW
+
