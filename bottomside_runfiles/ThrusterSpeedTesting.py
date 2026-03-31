d8217c68e8b90dc58ce272ef8adfb0ff0fbd0bac
gabby rosario <25Rosario@stfrancishs.org>
2026-03-31 13:33:00 -0700

    TestThrusterSpeed

---
+++ bottomside_runfiles/ThrusterSpeedTesting.py
@@ -0,0 +1,82 @@
+import pygame   #pip install pygame
+import board	#pip install board
+import busio	#pip install adafruit-blinka
+import adafruit_pca9685 #pip install adafruit-circuitpython-pca9685
+import time
+
+def main():
+	# library setup
+	pygame.init()
+
+	wait_time = 0.5
+	go_fwd = 1650
+	go_bak = 1450
+
+	i2c = busio.I2C(board.SCL, board.SDA)
+	shield = adafruit_pca9685.PCA9685(i2c)
+	shield.external_clock = True #enable 25MHz external crystal
+	shield.frequency = 96
+
+	thrusterChannels = [
+		shield.channels[8],  # Thruster 1
+		shield.channels[12], # Thruster 2
+		shield.channels[13], # Thruster 3
+		shield.channels[11], # Thruster 4
+		shield.channels[9],  # Thruster 5
+		shield.channels[14], # Thruster 6
+		shield.channels[15], # Thruster 7
+	]
+	thrusterChannels[0].duty_cycle = 0x2666
+
+	for channel in thrusterChannels:
+		throttlePW = int(2200/10000*65536)
+		channel.duty_cycle = throttlePW
+		time.sleep(0)
+		throttlePW = int(1500/10000*65536)
+		channel.duty_cycle = throttlePW
+		time.sleep(0)
+	
+	while True:
+		# Get thruster number from user
+		thruster_num = input("Enter thruster number (1-7) or 'q' to quit: ").strip()
+		
+		if thruster_num.lower() == 'q':
+			print("Exiting...")
+			break
+		
+		try:
+			thruster_num = int(thruster_num)
+			if thruster_num < 1 or thruster_num > 7:
+				print("Invalid thruster number. Please enter 1-7.")
+				continue
+		except ValueError:
+			print("Invalid input. Please enter a number 1-7 or 'q' to quit.")
+			continue
+		
+		# Test the selected thruster
+		channel = thrusterChannels[thruster_num - 1]
+		print(f"\nTesting Thruster {thruster_num}")
+		input("Press Enter to start thruster...")
+		
+		throttlePW = int(go_fwd/10000*65536)
+		channel.duty_cycle = throttlePW
+		print(f"Thruster {thruster_num} running forward")
+		time.sleep(wait_time)
+		
+		throttlePW = int(1500/10000*65536)
+		channel.duty_cycle = throttlePW
+		print(f"Thruster {thruster_num} stopped")
+		time.sleep(wait_time)
+		
+		throttlePW = int(go_bak/10000*65536)
+		channel.duty_cycle = throttlePW
+		print(f"Thruster {thruster_num} running backward")
+		time.sleep(wait_time)
+		
+		throttlePW = int(1500/10000*65536)
+		channel.duty_cycle = throttlePW
+		print(f"Thruster {thruster_num} stopped")
+		time.sleep(wait_time)
+
+if __name__ == "__main__":
+	main()
