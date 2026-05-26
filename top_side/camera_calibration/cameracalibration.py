279539fe4997444e55aab2ab55c9907ebd448512
cynthia-k2 <0907cincinnati@gmail.com>
2026-05-26 10:18:01 -0700

    yfyugi

--- top_side/camera_calibration/cameracalibration.py
+++ top_side/camera_calibration/cameracalibration.py
@@ -21,7 +21,7 @@ objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1,2)
 objPoints = [] # 3d point in real world space
 imgPoints = [] # 2d points in image plane.
 
-images = glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'C*.jpg'))
+images = glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'C*.jp*'))
 print(len(images))
 
 for image in images:
