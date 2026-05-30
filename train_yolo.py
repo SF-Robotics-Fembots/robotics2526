0e6d6a92015ce83d1f402be10ad70b6ad897a7e5
cynthia-k2 <0907cincinnati@gmail.com>
2026-05-30 16:36:12 -0700

    img rec updates

--- train_yolo.py
+++ train_yolo.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
-# Load a pretrained YOLO model (smallest version)
 model = YOLO("yolov8n.pt")
 
-# Train on your dataset
 model.train(
-    data=r"image_rec/Crab Detection.v1-crabs.yolov11/data.yaml",
+    data=r"image_rec/Crab Species Detection.v1i.yolov11/data.yaml",
     epochs=100,
     imgsz=640
 )
