a5fbaa8f5246f86dde15c5a72a28ba4072a04e08
SFHSRobotics <robotics@stfrancishs.org>
2026-06-16 11:05:06 -0700

    kills ports for cams to restart

---
+++ camera_bottomside/restartcams.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+bash cameraon.sh	
+
+#ok lets see if this fixes the error!
+# Kill any processes using cameras
+sudo fuser -k /dev/video0
+sudo fuser -k /dev/video2
+sudo fuser -k /dev/video4
+sudo fuser -k /dev/video6
+sudo fuser -k /dev/video8
+sudo fuser -k /dev/video10
+
+# Kill any processes using camera stream ports
+sudo fuser -k 8080/tcp
+sudo fuser -k 8082/tcp
+sudo fuser -k 8084/tcp
+sudo fuser -k 8086/tcp
+sudo fuser -k 8088/tcp
+sudo fuser -k 8090/tcp
+
+#camera on by default  2/28 i added sudo
+sudo rmmod uvcvideo
+sudo modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+bash /home/geneseas/robotics2526/camera_bottomside/focusscript.sh
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 0 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 2 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 4 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 6 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 8 & sleep 3
+bash /home/geneseas/robotics2526/camera_bottomside/camerarun.sh 10 & sleep 3
+
